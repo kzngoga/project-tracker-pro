259576f6e043dc09f03257abdf303687206eb953
Jovite Kz. Ngoga <kzngoga19@gmail.com>
2024-10-07 21:58:37 +0200

    chore: initial commit

---
+++ src/app/(Auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import { HomeIcon } from "lucide-react";
+
+export default function LoginPage() {
+  return (
+    <div className="p-5 flex items-center">
+      LoginPage <HomeIcon />
+    </div>
+  );
+}
